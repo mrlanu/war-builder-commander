89517cb8b009a6cd2474bc76790911a7aa79e0d3
mrlanu <mrlanu@gmail.com>
2020-01-13 21:28:41 -0600

    fanout exchange

--- src/main/java/io/lanu/travian/warbuildercommander/services/WarSenderServiceImpl.java
+++ src/main/java/io/lanu/travian/warbuildercommander/services/WarSenderServiceImpl.java
@@ -1,26 +1,25 @@
 package io.lanu.travian.warbuildercommander.services;
 
 import io.lanu.travian.warbuildercommander.models.CommandMessage;
-import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WarSenderServiceImpl implements WarSenderService{
 
     private RabbitTemplate template;
-    private DirectExchange directExchange;
+    private FanoutExchange fanoutExchange;
 
     public WarSenderServiceImpl(RabbitTemplate template,
-                                @Qualifier("direct") DirectExchange directExchange) {
+                                FanoutExchange fanoutExchange) {
         this.template = template;
-        this.directExchange = directExchange;
+        this.fanoutExchange = fanoutExchange;
     }
 
     @Override
     public void send(CommandMessage commandMessage) {
-        template.convertAndSend(directExchange.getName(), "attack", commandMessage);
+        template.convertAndSend(fanoutExchange.getName(), "attack", commandMessage);
         System.out.println(" [x] Sent attack - " + commandMessage.toString());
     }
 }

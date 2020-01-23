904c5e49f6cc53a85e8c40bb3c7947979158fb8e
mrlanu <mrlanu@gmail.com>
2020-01-22 21:55:30 -0600

    template.setReplyTimeout

--- src/main/java/io/lanu/travian/warbuildercommander/RabbitMqConfig.java
+++ src/main/java/io/lanu/travian/warbuildercommander/RabbitMqConfig.java
@@ -35,6 +35,7 @@ public class RabbitMqConfig {
     {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(messageConverter());
+        template.setReplyTimeout(20000);
         return template;
     }
 
